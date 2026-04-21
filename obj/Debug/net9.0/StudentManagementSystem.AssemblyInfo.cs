ad52cb3e0925b55f70906a7e8877d4084bb2bc13
krishn kumari <mrk.kumari@gmail.com>
2026-04-21 18:57:35 +0530

    Enabled SQL retry policy

--- obj/Debug/net9.0/StudentManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b46952f048aecd73e15c179952f9547d79df173")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
