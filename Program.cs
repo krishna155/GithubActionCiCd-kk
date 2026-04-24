344fe857597466b0d1f8ae4274ff861769f9e207
krishn kumari <mrk.kumari@gmail.com>
2026-04-24 12:24:31 +0530

    program.cs modify

--- Program.cs
+++ Program.cs
@@ -15,11 +15,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Students}/{action=Index}/{id?}");
 
 app.Run();
 // using Microsoft.EntityFrameworkCore;
